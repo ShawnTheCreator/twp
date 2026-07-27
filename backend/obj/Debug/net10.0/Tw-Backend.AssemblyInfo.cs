5d3d8c0b5f5a09bc6b2bbf0f71340cbc38b0169c
ShawnTheCreator <shawn@example.com>
2026-07-27 16:56:33 +0200

    emails3

--- backend/obj/Debug/net10.0/Tw-Backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/Tw-Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tw-Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11179b853857d9e1e4c283a6b70fb24ccc305720")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53abdeea1708bf230d4e2b4c735743e635abeab2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tw-Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tw-Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
